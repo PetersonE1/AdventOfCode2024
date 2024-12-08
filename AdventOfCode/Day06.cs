12392dfda4411fc3aa2ca217b871df622d25b176
PetersonE1 <PetersonE_1@live.com>
2024-12-08 01:10:07 -0500

    day 6 part 1

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode
 {
-    [RunTest]
+    //[RunTest]
     public class Day06 : TestableDay
     {
         private readonly string _input;
@@ -31,16 +31,68 @@ namespace AdventOfCode
                     {
                         _start.x = x;
                         _start.y = y;
+                        _visitedMap[x, y] = true;
                     }
                 }
             }
         }
 
+        private enum Direction
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
+
         public override ValueTask<string> Solve_1()
         {
-            PrintMap();
+            (int x, int y) position = _start;
+            Direction direction = Direction.Up;
 
-            return new ValueTask<string>(string.Empty);
+            while (true)
+            {
+                (int x, int y) prospectivePos = direction switch
+                {
+                    Direction.Up => (position.x, position.y - 1),
+                    Direction.Down => (position.x, position.y + 1),
+                    Direction.Left => (position.x - 1, position.y),
+                    Direction.Right => (position.x + 1, position.y),
+                    _ => throw new InvalidOperationException()
+                };
+                if (prospectivePos.x < 0 || prospectivePos.x >= _map.GetLength(0) || prospectivePos.y < 0 || prospectivePos.y >= _map.GetLength(1))
+                {
+                    break;
+                }
+
+                if (!_map[prospectivePos.x, prospectivePos.y])
+                {
+                    _visitedMap[prospectivePos.x, prospectivePos.y] = true;
+                    position = prospectivePos;
+                }
+                else
+                {
+                    direction = direction switch
+                    {
+                        Direction.Up => Direction.Right,
+                        Direction.Right => Direction.Down,
+                        Direction.Down => Direction.Left,
+                        Direction.Left => Direction.Up,
+                        _ => throw new InvalidOperationException()
+                    };
+                }
+            }
+
+            int numVisited = 0;
+            foreach (bool visited in _visitedMap)
+            {
+                if (visited)
+                {
+                    numVisited++;
+                }
+            }
+
+            return new ValueTask<string>(numVisited.ToString());
         }
 
         public override ValueTask<string> Solve_2()
